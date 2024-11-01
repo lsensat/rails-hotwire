e5554759a0dcd5bdaf4b92a6e60e40f674e8a7b9
luis sensat <luis.sensat@gmail.com>
2024-11-01 20:10:52 +0100

    added controller Quotes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,2 +1,50 @@
 class QuotesController < ApplicationController
+  before_action :set_quote, only: i%[show, edit, update, destroy]
+
+  def index
+    @quotes = Quote.all
+  end
+
+  def show
+    #show
+  end
+
+  def new
+    @quote = Quote.new
+  end
+
+  def create
+    @quote = Quote.new(quote_params)
+
+    if @quote.save
+      redirect_to quote_path, notice: "Quote was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def edit
+    #edit
+  end
+
+  def uptate
+    if @quote.update(quote_params)
+      redirect_to quotes_path, notice: "Quote was successfully updated."
+    end
+  end
+
+  def destroy
+    @quote.destroy
+    redirect_to quotes_path, notice: "Quote was successfuly destroyed."
+  end
+
+  private
+
+  def set_quote
+    @quote = Quote.find(params[:id])
+  end
+
+  def quotes_params
+    params.require(:quote).permit(:name)
+  end
 end
