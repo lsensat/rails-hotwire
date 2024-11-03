ab61414de71165a009fc4ea6027bbed5648f0923
luis sensat <luis.sensat@gmail.com>
2024-11-03 15:53:34 +0100

    style working

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import * as bootstrap from "bootstrap"
-import "controllers"
+import "./controllers"
 import "@popperjs/core"
