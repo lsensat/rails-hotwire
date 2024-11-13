e57a3d43374f932c52c3ede5d3e26dd184f3666f
luis sensat <luis.sensat@gmail.com>
2024-11-13 20:05:46 +0100

    added flash on destroy action

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -41,8 +41,8 @@ class QuotesController < ApplicationController
     @quote.destroy
 
     respond_to do |format|
-      format.html { redirect_to quotes_path, notice: "Quote was successfuly destroyed." }
-      format.turbo_stream
+      format.html { redirect_to quotes_path, notice: "Quote was successfully destroyed." }
+      format.turbo_stream { flash.now[:notice] = "Quote was successfully destroyed." }
     end
   end
 
