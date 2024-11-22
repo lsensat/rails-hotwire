3a6c21a6513172ebdcea8b36cf2bd904d58dee96
luis sensat <luis.sensat@gmail.com>
2024-11-22 20:24:45 +0100

    ordering new item dates

--- app/controllers/line_item_dates_controller.rb
+++ app/controllers/line_item_dates_controller.rb
@@ -27,6 +27,19 @@ class LineItemDatesController < ApplicationController
     end
   end
 
+  def create
+    @line_item_date = @quote.line_item_dates.build(line_item_date_params)
+
+    if @line_item_date.save
+      respond_to do |format|
+        format.html { redirect_to quote_path(@quote), notice: "Date was successfully created." }
+        format.turbo_stream { flash.now[:notice] = "Date was successfully created." }
+      end
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     @line_item_date.destroy
 
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
   # root "posts#index"
   root "pages#home"
 
-  resources :quotes
   resources :quotes do
     resources :line_item_dates, except: [:index, :show]
   end
